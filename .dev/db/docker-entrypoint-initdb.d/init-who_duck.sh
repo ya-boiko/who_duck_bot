9268f8f3cf57fb1fec52b540c22895e035f0ff3a
Vlad Boiko <acc.v.boiko@gmail.com>
2024-12-22 07:57:05 +0100

    updated docker-compose.yml

--- .dev/db/docker-entrypoint-initdb.d/init-who_duck.sh
+++ .dev/db/docker-entrypoint-initdb.d/init-who_duck.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+psql -v ON_ERROR_STOP=1 --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" <<-EOSQL
+	CREATE EXTENSION IF NOT EXISTS vector;
+EOSQL
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE who_duck_test;
+	CREATE DATABASE who_duck__db_test;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname who_duck__db_test <<-EOSQL
+	CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
