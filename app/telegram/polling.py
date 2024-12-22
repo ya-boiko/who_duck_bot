92876c5f3a6c13f4e3b150fb8e576ddd17b8058a
Vlad Boiko <acc.v.boiko@gmail.com>
2024-12-22 08:07:42 +0100

    updated telegram polling

--- app/telegram/polling.py
+++ app/telegram/polling.py
@@ -1,14 +1,17 @@
 """Telegram."""
 
-import tempfile
-
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from dependency_injector.wiring import Provide, inject
 
+from app.adapters.orm import bind_mappers
 from app.domain import commands
 from app.service_layer.message_bus import MessageBus
+from app.settings import Settings
+
+
+bind_mappers()
 
 
 @inject
@@ -16,29 +19,27 @@ async def tg_polling(
     dp: Dispatcher = Provide['dp'],
     bot: Bot = Provide['bot'],
     bus: MessageBus = Provide['bus'],
+    settings: Settings = Provide['settings'],
 ):
+
+    def message_from_admin(message: Message):
+        return str(message.from_user.id) in settings.app.admins
 
     @dp.message(CommandStart())
     async def command_start_handler(message: Message) -> None:
         """The handler receives messages with `/start` command."""
         await message.answer(f'Hello, {html.bold(message.from_user.full_name)}!')
 
-
     @dp.message(F.document)
     async def save_document(message: Message):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            cmd = commands.DownloadFileToDir(
-                file_id=message.document.file_id,
-                dir=temp_dir
-            )
-            temp_file_path = await bus.handle(cmd).pop()
+        if not message_from_admin(message):
+            return None
 
-            with open(temp_file_path, 'rb') as temp_file:
-                cmd = commands.UploadFile(
-                    file_path=temp_file_path,
-                )
-                filename = await bus.handle(cmd).pop()
+        cmd = commands.SaveImage(
+            file_id=message.document.file_id
+        )
+        text = await bus.handle(cmd).pop()
 
-            await message.reply(f'Изображение сохранено как {filename}')
+        await message.reply(text)
 
     await dp.start_polling(bot)
