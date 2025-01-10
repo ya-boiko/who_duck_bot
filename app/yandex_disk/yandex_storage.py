3646cb7d658443d5135688023bd14f95fdacefe7
Vlad Boiko <acc.v.boiko@gmail.com>
2025-01-10 20:29:36 +0100

    updated YandexStorage class
    added full_path property and download() method and updated mkdir(), _set_app_dir() and _create_app_dirs_if_not_exists() methods

--- app/yandex_disk/yandex_storage.py
+++ app/yandex_disk/yandex_storage.py
@@ -28,6 +28,10 @@ class YandexStorage:
 
         self._full_path = self.main_dir + self.app_dir
 
+    @property
+    def full_path(self) -> str:
+        return self._full_path
+
     def client(self):
         return AsyncClient(
             token=self.token,
@@ -45,13 +49,26 @@ class YandexStorage:
             print(await client.check_token())
             await client.upload(file, self._full_path + '/' + filename, timeout=300)
 
+    async def download(self, filename: str, dest_path: str) -> None:
+        """Downloads file from the storage.
+
+        :param filename: name for the file to be downloaded.
+        :param dest_path: destination path.
+        """
+        async with self.client() as client:
+            print(await client.check_token())
+            
+            src_path = self.full_path  + (filename if self.full_path.endswith("/") else "/" + filename)
+            path_ = dest_path  + (filename if dest_path.endswith("/") else "/" + filename)
+            await client.download(src_path, path_, timeout=300)
+
     async def mkdir(self, dir_path: str, client: Optional[AsyncClient] = None) -> None:
         if client:
-            await client.mkdir(dir_path)
+            await client.mkdir(self._full_path + '/' + dir_path)
             return
 
         async with self.client() as client:
-            await client.mkdir(dir_path)
+            await client.mkdir(self._full_path + '/' + dir_path)
 
     async def ls(self, dir_path, client: Optional[AsyncClient] = None) -> list[str]:
         if client:
@@ -75,22 +92,24 @@ class YandexStorage:
         if value.startswith('/'):
             value = value[1:]
 
+        if value.endswith('/'):
+            value = value[:-1]
+
         self.app_dir = value
+        self._app_dir_exists = False
 
     async def _create_app_dirs_if_not_exists(self, client: AsyncClient) -> None:
         async with self.lock:
             if self._app_dir_exists:
                 return
 
-            app_dirs = self.app_dir.split('/')
-            path = self.main_dir[:-1]
-            for app_dir in [d for d in app_dirs if d]:
-                if app_dir in await self.ls(path, client):
-                    path += f'/{app_dir}'
-                    continue
+            path = self.main_dir
+            for dir_name in [d for d in self.app_dir.split("/") if d]:
+                dir_path = path + (dir_name if path.endswith("/") else "/" + dir_name)
+                if dir_name not in await self.ls(path, client):
+                    await client.mkdir(dir_path)
+                    print(f'Директория {dir_path} создана.')
 
-                path += f'/{app_dir}'
-                await self.mkdir(path, client)
-                print(f'Директория {path} создана.')
+                path = dir_path
 
             self._app_dir_exists = True
