f371a0aba1d870886d3034c44d7e6e31665b0d66
Vlad Boiko <acc.v.boiko@gmail.com>
2025-01-10 20:24:14 +0100

    added domain commands - yandex_commands.DownloadFile and ai_commands.FindCloseImages

--- app/domain/commands/__init__.py
+++ app/domain/commands/__init__.py
@@ -2,6 +2,12 @@
 
 from .command import Command
 from .tg_commands import DownloadFileToDir
-from .yandex_commands import UploadFile
+from .yandex_commands import UploadFile, DownloadFile
 from .bot_commands import SaveImage, StartDialog, FinishDialog
-from .ai_commands import GenerateEmbedding, GenerateDocumentDescription, GenerateAnswer, GenerateWhiningAnswer
+from .ai_commands import (
+    GenerateEmbedding,
+    GenerateDocumentDescription,
+    GenerateAnswer,
+    GenerateWhiningAnswer,
+    FindCloseImages
+)
